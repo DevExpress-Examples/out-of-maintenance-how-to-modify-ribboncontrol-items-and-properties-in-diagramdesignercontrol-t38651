6f1d811bff97b233bc0442cece6bc4a91a4d6d2c
DevExpressExampleBot <fakeEmail>
2022-12-17 08:24:40 +0400

    Source auto update [skip ci]

--- VB/DXDiagram.CustomizeRibbon/MainWindow.xaml.vb
+++ VB/DXDiagram.CustomizeRibbon/MainWindow.xaml.vb
@@ -1,25 +1,13 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports DevExpress.Xpf.Core
 
 Namespace DXDiagram.CustomizeRibbon
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
